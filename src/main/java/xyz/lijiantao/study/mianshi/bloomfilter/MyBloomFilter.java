da9205610e7d0b9d7aacc8d1117202a43a66c8ae
myskybeyond <vlijiantao@163.com>
2020-04-01 22:45:25 +0800

    add note

--- src/main/java/xyz/lijiantao/study/mianshi/bloomfilter/MyBloomFilter.java
+++ src/main/java/xyz/lijiantao/study/mianshi/bloomfilter/MyBloomFilter.java
@@ -3,6 +3,8 @@ package xyz.lijiantao.study.mianshi.bloomfilter;
 import java.util.BitSet;
 
 /**
+ * 布隆过滤器
+ *
  * @author Jiantao Li
  * @date 2020/2/12 9:59
  */
@@ -16,7 +18,7 @@ public class MyBloomFilter {
     /**
      * 通过这个数组可以创建6个不同的哈希函数
      */
-    private final static int[] SEEDS = new int[] {3,13,46,71,91,134};
+    private final static int[] SEEDS = new int[]{3, 13, 46, 71, 91, 134};
 
     /**
      * 位数组。数组中的元素只能是0或1
@@ -31,18 +33,18 @@ public class MyBloomFilter {
     /**
      * 初始化多个包含hash函数的类的数组，每个类中的hash函数都不一样
      */
-    public MyBloomFilter(){
+    public MyBloomFilter() {
         for (int i = 0; i < SEEDS.length; i++) {
-            func[i] = new SimpleHash(DEFAULT_SIZE,SEEDS[i]);
+            func[i] = new SimpleHash(DEFAULT_SIZE, SEEDS[i]);
         }
     }
 
     /**
      * 添加元素到位数组
      */
-    public void add(Object value){
-        for (SimpleHash f: func){
-            bits.set(f.hash(value),true);
+    public void add(Object value) {
+        for (SimpleHash f : func) {
+            bits.set(f.hash(value), true);
         }
     }
 
@@ -50,19 +52,19 @@ public class MyBloomFilter {
     /**
      * 判断指定元素是否包含在位数组
      */
-    public boolean contains(Object value){
+    public boolean contains(Object value) {
         boolean ret = true;
-        for(SimpleHash f : func){
+        for (SimpleHash f : func) {
             ret = ret && bits.get(f.hash(value));
         }
         return ret;
     }
 
-    public static class SimpleHash{
+    public static class SimpleHash {
         private int cap;
         private int seed;
 
-        public SimpleHash(int cap,int seed){
+        public SimpleHash(int cap, int seed) {
             this.cap = cap;
             this.seed = seed;
         }
@@ -70,9 +72,9 @@ public class MyBloomFilter {
         /**
          * 计算hash值
          */
-        public int hash(Object value){
+        public int hash(Object value) {
             int h;
-            return (value == null) ? 0 : Math.abs(seed * (cap -1) & ((h = value.hashCode())^ (h >>> 16)));
+            return (value == null) ? 0 : Math.abs(seed * (cap - 1) & ((h = value.hashCode()) ^ (h >>> 16)));
         }
 
     }
